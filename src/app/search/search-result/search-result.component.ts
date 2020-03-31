df0063832402a798cec4797d6a2e864936baeb2e
carmelo <carmelo.besiduw@gmail.com>
2020-03-31 12:53:02 +0800

    [Add] Add search-result component

---
+++ src/app/search/search-result/search-result.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-result',
+  templateUrl: './search-result.component.html',
+  styleUrls: ['./search-result.component.css']
+})
+export class SearchResultComponent implements OnInit {
+
+  public recipes: {id: number, title: string , publisher: string, url: string, img: string, ingredients: []}[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    this.recipes = [
+      {
+        id:23456,
+        title:'Pasta with Tomato ...',
+        publisher:'The Pioneer Woman',
+        url: '',
+        img: '../assets/img/test-1.jpg',
+        ingredients: []
+      },
+      {
+        id:76767,
+        title:'Pasta Salad with ...',
+        publisher:'Spicy Perspective',
+        url: '',
+        img: '../assets/img/test-2.jpg',
+        ingredients: []
+      },
+      {
+        id:85354,
+        title:'Homemade Tomato ...',
+        publisher:'All Recipes',
+        url: '',
+        img: '../assets/img/test-3.jpg',
+        ingredients: []
+      },
+      {
+        id:43563,
+        title:'Pasta with Tomato ...',
+        publisher:'The Pioneer Woman',
+        url: '',
+        img: '../assets/img/test-4.jpg',
+        ingredients: []
+      },
+      {
+        id:2256665,
+        title:'Greek Pasta with ...',
+        publisher:'Chow',
+        url: '',
+        img: '../assets/img/test-5.jpg',
+        ingredients: []
+      },
+  ]
+  }
+
+}
